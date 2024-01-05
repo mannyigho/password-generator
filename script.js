fc7df4cf6a9d0b6a4afab37a081b635e93e8c96e
Igho Manny Ibuje <imigreat@yahoo.com>
2024-01-05 23:33:58 +0000

    Password generator completed in script.js file

--- script.js
+++ script.js
@@ -88,20 +88,133 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+
+function specialChars(){
+  var specialX;  
+  var specialXs = '';
+  if (confirm("Would you add a special character to your password?")) {
+    specialXs = "1";
+  }else{
+    specialXs = "2";
+  }
+
+  if(specialXs == "1"){
+    for(i=0; i < specialCharacters.length; i++){
+      specialX += specialCharacters[i];
+    }
+    return specialX.replace("undefined", "");
+  }else{
+    return specialX = "";
+  }
+ 
+}
+
+
+function specialNums(){
+  var specialN;
+  var specialNs = '';
+  if (confirm("Would you add a numeric number between 0 - 9 to your password?")) {
+    specialNs = "1";
+  }else{
+    specialNs = "2";
+  }
+
+  if(specialNs == "1"){
+  for(j = 0; j < numericCharacters.length; j++){
+    specialN += numericCharacters[j];
+  }
+    return specialN.replace("undefined", "");
+  }else{
+    return specialN = "";
+  }
+}
+
+
+function lowerChars(){
+  var specialL;
+  var specialLs = '';
+  if (confirm("Would you add a lower case letter between a - z to your password?")) {
+    specialLs = "1";
+  }else{
+    specialLs = "2";
+  }
+  
+  if(specialLs == "1"){
+  for(k = 0; k < lowerCasedCharacters.length; k++){
+    specialL += lowerCasedCharacters[k];
+  }
+    return specialL.replace("undefined", "");
+  }else{
+    return specialL = "";
+  }
+}
+
+
+function upperChars(){
+  var specialU;
+  var specialUs = '';
+  if (confirm("Would you add an upper case letter between A - Z to your password?")) {
+    specialUs = "1";
+  }else{
+    specialUs = "2";
+  }
+
+  if(specialUs == "1"){
+  for(l = 0; l < upperCasedCharacters.length; l++){
+    specialU += upperCasedCharacters[l];
+  }
+    return specialU.replace("undefined", "");
+  }else{
+    return specialU = "";
+  }
+}
+
+
 // Function to prompt user for password options
 function getPasswordOptions() {
+  var sp = this.specialChars();
+  var sn = this.specialNums();  
+  var lc = this.lowerChars();
+  var uc = this.upperChars();
 
+  if(sp.length > 0 && sn.length > 0 && lc.length > 0 && uc.length > 0){
+    return uc + lc + sn + sp;
+  }else{
+    return "";
+  }
+  
 }
+
 
 // Function for getting a random element from an array
+var arr = getPasswordOptions()
 function getRandom(arr) {
-
+  if(arr.length > 0){
+    return arr;
+  }else{
+    return "";
+  }
 }
+
 
 // Function to generate password with user input
 function generatePassword() {
+  var stringLength = 15;
+  var randomstring = '';
+  var chars = getRandom(arr);
+  //console.log(characters);
+  if(chars.length > 0) {
+    for (var i = 0; i < stringLength; i++) {
+      var rnum = Math.floor(Math.random() * chars.length);
+      randomstring += chars[rnum];
+  }
+    return randomstring;
+  }else{
+    return "Invalid";
+  }
 
 }
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
@@ -110,8 +223,11 @@ var generateBtn = document.querySelector('#generate');
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
-
-  passwordText.value = password;
+  if(password === 'Invalid'){
+    passwordText.value = "Invalid! All requirements are to be met.";
+  }else{
+    passwordText.value = password;
+  }
 }
 
 // Add event listener to generate button
